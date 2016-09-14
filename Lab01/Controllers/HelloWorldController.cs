8dbcc8e0b38bb027b93a46ff1e345df71d1b12cc
Trần Nhật Duy <tui@duydev.me>
2016-09-14 16:58:43 +0700

    Add project files.

---
+++ Lab01/Controllers/HelloWorldController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab01.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public String Hello(string name, int age)
+        {
+            return "Xin chào bạn " + name + ", " + age + "tuổi.";
+        }
+    }
+}
