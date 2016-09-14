3910ea18456839b249a1844d6b96532c6cfcc4e5
Trần Nhật Duy <tui@duydev.me>
2016-09-14 18:02:57 +0700

    @Add Dashboard

---
+++ Lab01/Controllers/BookStoreController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Lab01.Models;
+
+namespace Lab01.Controllers
+{
+    public class BookStoreController : Controller
+    {
+        dbQLBanSachDataContext data = new dbQLBanSachDataContext();
+
+        private List<SACH> LaySachMoi(int count)
+        {
+            // Lay count sach giam dan theo ngay cap nhat
+            return data.SACHes.OrderByDescending(a => a.NgayCapNhat).Take(count).ToList();
+        }
+
+        // GET: BookStore
+        public ActionResult Index()
+        {
+            List<SACH> SachMoi = LaySachMoi(5);
+
+            return View();
+        }
+
+
+    }
+}
---
+++ Lab01/Controllers/DashboardController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Lab01.Models;
+
+namespace Lab01.Controllers
+{
+    public class DashboardController : Controller
+    {
+
+
+        // GET: Dashboard
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult AddAdmin()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddAdmin()
+        {
+            return View();
+        }
+    }
+}
